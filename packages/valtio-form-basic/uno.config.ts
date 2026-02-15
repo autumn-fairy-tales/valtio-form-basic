8946a00f3180da2944ceaabaedd2660d48a470d3
SunLxy <1011771396@qq.com>
2026-02-15 18:31:14 +0800

    fix: 修改样式rem转换px

--- packages/valtio-form-basic/uno.config.ts
+++ packages/valtio-form-basic/uno.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'unocss';
+import presetRemToPx from '@unocss/preset-rem-to-px';
 import { presetWind3 } from '@unocss/preset-wind3';
 
 export default defineConfig({
   content: {
     filesystem: ['./src/**/*.{js,ts,jsx,tsx}'],
   },
-  presets: [presetWind3({ prefix: 'fairystaroform__' })],
+  presets: [presetRemToPx({}), presetWind3({ prefix: 'fairystaroform__' }) as any],
 });
